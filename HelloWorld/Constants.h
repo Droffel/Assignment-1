4b4250bf92846d345a3f0fb81a1ca84ff11cc4bf
Droffel <Filip.n0525@gmail.com>
2025-12-02 15:35:43 +0100

    commit 5

--- HelloWorld/Constants.h
+++ HelloWorld/Constants.h
@@ -5,7 +5,6 @@ const int DISPLAY_HEIGHT = 360;
 const float BRICKHALFWIDTH = 8.0f;
 const float BRICKWIDTH = 5.0f;
 const int DISPLAY_SCALE = 2;
-
 const float SPEED = 6.0f;
 const float PADDLEWIDTH = 60.0f;
 const float PADDLEHEIGHT = 10.0f;
